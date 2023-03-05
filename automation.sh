d595b342daf34df104a18ff8980a88250dad5469
rishabhtya9i <42796918+rishabhtya9i@users.noreply.github.com>
2023-03-05 14:36:58 -0500

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -5,6 +5,8 @@ timestamp=$(date '+%d%m%Y-%H%M%S')
 apache_logs_dir="/var/log/apache2/"
 tar_file_name="${myname}-httpd-logs-${timestamp}.tar"
 tar_file_path="/tmp/${tar_file_name}"
+inventory_file="/var/www/html/inventory.html"
+
 
 sudo apt update -y
 
@@ -18,8 +20,22 @@ then
     sudo systemctl start apache2
 fi
 
+# create inventory file if it does not exist
+if [ ! -f $inventory_file ]; then
+    echo -e "Log Type\tDate Created\tType\tSize" > $inventory_file
+fi
+
 sudo systemctl enable apache2
 
-sudo tar -cvf ${tar_file_path} ${apache_logs_dir}*.log --exclude "*.tar" --exclude "*.zip" &> /dev/null
+sudo tar -cvf ${tar_file_path} ${apache_logs_dir}*.log &> /dev/null
+
+backup_size=$(du -h ${tar_file_path} | awk '{print $1}')
+
+echo -e "httpd-logs\t$timestamp\ttar\t$backup_size" >> $inventory_file
+
 
 aws s3 cp ${tar_file_path} s3://${s3_bucket}/${tar_file_name}
+
+if [ ! -f /etc/cron.d/automation ]; then
+    echo "0 0 * * * root /root/Automation_Project/automation.sh" > /etc/cron.d/automation
+fi
